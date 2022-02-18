9fa80d6509d26e6f6b1393e6c37a23fcc31b0db4
leonardosartori-web <79639983+leonardosartori-web@users.noreply.github.com>
2022-02-18 16:20:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -10,3 +10,12 @@ def index():
 	posts = connection.execute('SELECT * FROM posts').fetchall()
 	connection.close()
 	return render_template('index.html', posts=posts)
+
+@app.route('/<int:idx>/delete', method=('POST',))
+def delete(idx):
+	connection = sqlite3.connect('database.db')
+	connection.row_factory = sqlite3.Row
+	posts = connection.execute('DELETE FROM posts WHERE id=?', (idx,))
+	connection.commit()
+	connection.close()
+	return redirect('/')
